76f2e154dcc2d1b56e8c0edb6de2852e8bf01008
Tammy <misstamar@gmail.com>
2015-11-10 14:55:58 -0800

    added show method and route

--- task-list/db/schema.rb
+++ task-list/db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110002025) do
+ActiveRecord::Schema.define(version: 20151110223044) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.boolean  "complete"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "completed_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
