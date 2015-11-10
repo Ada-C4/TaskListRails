28e01a2d27b23caf56807a138ebd4066236d4bd7
Wesley Willis <haileyw@gmail.com>
2015-11-10 13:59:36 -0800

    primary requirements without styling

--- task-list/config/routes.rb
+++ task-list/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-get 'tasks/' => 'tasks#index'
+  get 'tasks/' => 'tasks#index'
+
+  get 'tasks/:id' => 'tasks#show'
+
+  get 'posts/:id/edit' => 'posts#edit'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
