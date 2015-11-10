3079408776278b909de441d810913888cb9ebc76
Daphne <dg1181@nyu.edu>
2015-11-10 13:32:31 -0800

    Migrated column_not_null for :name

--- task-list/db/schema.rb
+++ task-list/db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110000549) do
+ActiveRecord::Schema.define(version: 20151110212956) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",           null: false
     t.string   "description"
     t.datetime "completed_date"
     t.datetime "created_at",     null: false
