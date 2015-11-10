6dbf1477fbc0b8713e01cefd4994f2501dfa3ecb
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-09 16:56:01 -0800

    deleted index content and formatting, only basic text now

--- task-list-rails/config/routes.rb
+++ task-list-rails/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks' => 'tasks#index' 
+  get 'tasks/' => 'tasks#index' 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
