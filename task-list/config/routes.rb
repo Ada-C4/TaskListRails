814220289ccef1133581647c912022e1b5e35f83
Desiree <desireepoland@gmail.com>
2015-11-10 14:41:45 -0800

    Create a show route and view

--- task-list/config/routes.rb
+++ task-list/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'tasks#index'
+  get 'tasks/:id' => 'tasks#show'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
