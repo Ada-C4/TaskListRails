766f85003d7678d991330eb6db0386187bb8aa08
Desiree <desireepoland@gmail.com>
2015-11-10 16:54:34 -0800

    Add styling, reword show view

--- task-list/app/controllers/tasks_controller.rb
+++ task-list/app/controllers/tasks_controller.rb
@@ -1,9 +1,11 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+    @title= "Task List"
   end
   def show
     id = params[:id]
     @task = Task.find(id)
+    @title = @task.name
   end
 end
