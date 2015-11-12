57a61ba4c599a2df891270b8591f758078fb57ba
Annalee <annaleeherrera@gmail.com>
2015-11-11 17:51:09 -0800

    adding css

--- tasklist/db/seeds.rb
+++ tasklist/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+def random_time
+  Time.at(rand * Time.now.to_i)
+end
+
+tasks = [
+  { name: "The First Task", descript: "do this first", date_completed: random_time },
+  { name: "Go to Brunch", descript: "go to Cafe Presse" },
+  { name: "Go to Lunch", descript: "eat", date_completed: random_time },
+  { name: "Go to Second Lunch", descript: "eat more" },
+  { name: "Play Video Games", descript: "Super Mario", date_completed: random_time },
+  { name: "High Five Somebody You Don't Know", description: "Everyone", date_completed: random_time },
+  { name: "Plant Flowers", descript: "Magnolias!", date_completed: random_time },
+  { name: "Call Mom", descript: "Hi MOM" },
+  { name: "She worries, you know.", descript: "Yup" },
+  { name: "Nap.", descript: "", date_completed: random_time }
+]
+
+tasks.each do |task|
+  Task.create(task)
+end
