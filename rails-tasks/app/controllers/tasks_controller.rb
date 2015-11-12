94a743c73a9422ac33259f16edd3bea16e3030c5
Lauren Granger <lr.granger21@gmail.com>
2015-11-11 16:59:30 -0800

    index tasks link to description view

--- rails-tasks/app/controllers/tasks_controller.rb
+++ rails-tasks/app/controllers/tasks_controller.rb
@@ -1,7 +1,14 @@
+require "pry"
+
 class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
+  end
+
+  def show
+    @task_name = params[:name]
+    @description = Task.where(name: @task_name)[0].description
   end
 
 end
