c04cd4642ef28aeb53cd1b28906080bc63e3b554
Desiree <desireepoland@gmail.com>
2015-11-12 13:43:25 -0800

    Allow users to create new tasks

--- task-list/app/controllers/tasks_controller.rb
+++ task-list/app/controllers/tasks_controller.rb
@@ -8,4 +8,15 @@ class TasksController < ApplicationController
     @task = Task.find(id)
     @title = @task.name
   end
+  def new
+    @title = "Create A New Task"
+    @task = Task.new
+  end
+  def create
+    @task = Task.create(task_params)
+    redirect_to "/"
+  end
+  def task_params
+    params.require(:task).permit(:name, :description, :completed_at)
+  end
 end
