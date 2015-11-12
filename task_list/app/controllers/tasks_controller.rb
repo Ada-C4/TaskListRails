ab18661191adb81bccac420ee021d88d87ffebc0
Meighan Rasley <meighanr@gmail.com>
2015-11-11 21:17:05 -0800

    Wave 1

--- task_list/app/controllers/tasks_controller.rb
+++ task_list/app/controllers/tasks_controller.rb
@@ -1,2 +1,13 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+    @title = "Tasks"
+  end
+
+  def show
+    id = params[:id]
+    @task = Task.find(id)
+    @title = "Task Info"
+  end
 end
--- task_list/db/schema.rb
+++ task_list/db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110001336) do
+ActiveRecord::Schema.define(version: 20151110221757) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "completed_date"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "completed_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.boolean  "completed"
   end
 
 end
