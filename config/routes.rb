f46f14e105e131247422c47980384db56ec121b6
Sarah <trowbrsa@gmail.com>
2015-11-11 21:50:20 -0800

    added additional css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+#tasks points to the
+  get '/' => 'tasks#index'
+  get '/:id' => 'tasks#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
