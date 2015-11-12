fe03dd00e9204870ef4992364774b6b4789444e4
Annalee <annaleeherrera@gmail.com>
2015-11-12 15:29:44 -0800

    wave 2, not done w/ css

--- tasklist/app/controllers/tasks_controller.rb
+++ tasklist/app/controllers/tasks_controller.rb
@@ -7,4 +7,28 @@ class TasksController < ApplicationController
     id = params[:id]
     @task = Task.find(id)
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params[:task])
+
+  # OR
+  #
+  # t= Task.new(param[:task])
+  # t.save
+    redirect_to "/"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+    redirect_to "/tasks"
+  end
+  private
+
+  def task_params
+    params.permit(task:[:name, :descript, :date_completed])
+  end
 end
