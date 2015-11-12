2d310caf801996cd3e291467dd9522695efc4eb5
Lauren Granger <lr.granger21@gmail.com>
2015-11-11 18:33:48 -0800

    css and good looking datetime rendering

--- rails-tasks/app/controllers/tasks_controller.rb
+++ rails-tasks/app/controllers/tasks_controller.rb
@@ -8,7 +8,16 @@ class TasksController < ApplicationController
 
   def show
     @task_name = params[:name]
-    @description = Task.where(name: @task_name)[0].description
+    task = Task.where(name: @task_name)[0]
+    @description = task.description
+    @completed_status = task.complete
+    date = task.completed_date
+    binding.pry
+    if date == nil
+      @completed_date = nil
+    else
+      @completed_date = "#{date.month}/#{date.day}/#{date.year}"
+    end
   end
 
 end
--- rails-tasks/db/schema.rb
+++ rails-tasks/db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110002439) do
+ActiveRecord::Schema.define(version: 20151112011521) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.boolean  "complete"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.datetime "completed_date"
   end
 
 end
