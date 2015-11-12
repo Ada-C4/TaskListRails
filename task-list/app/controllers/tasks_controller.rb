15f8006435996fd317d0b4c6654a05ec56d024fc
Dreedle <davis.audrey.lynn@gmail.com>
2015-11-12 12:19:05 -0800

    Can add a new task and it will be added to the database. Redirects to tasks index.

--- task-list/app/controllers/tasks_controller.rb
+++ task-list/app/controllers/tasks_controller.rb
@@ -1,12 +1,25 @@
 class TasksController < ApplicationController
 
+  def create
+    Task.create(task_params[:task])
+    redirect_to "/tasks/"
+  end
+
   def index
     @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
   end
 
   def show
     id = params[:id]
     @task = Task.find(id)
+  end
+
+  def task_params
+    params.permit(task:[:name, :description, :completed_date])
   end
 
 end
--- task-list/config/routes.rb
+++ task-list/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   #
   root 'welcome#index'
   get 'tasks/' => 'tasks#index'
+  get 'tasks/new' => 'tasks#new'
   get 'tasks/:id' => 'tasks#show'
+  post 'tasks/' => 'tasks#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
