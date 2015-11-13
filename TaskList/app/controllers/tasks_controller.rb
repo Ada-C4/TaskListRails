2eda0d5a43ef3372606eb0eac7ef47b730a575a0
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-13 09:37:47 -0800

    Edit Button created, route functioning'

--- TaskList/app/controllers/tasks_controller.rb
+++ TaskList/app/controllers/tasks_controller.rb
@@ -26,12 +26,12 @@ class TasksController < ApplicationController
 	end
 
 	def edit
-		
+		@task = Task.find(params[:id])
 	end
 
 	def update
 		# if params[:completed] 
-		redirect_to 
+		redirect_to '/'
 	end
 
 	private
