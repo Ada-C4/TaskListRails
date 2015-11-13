8203f7134e98800e15b468a3d4eb0dd417585977
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-13 15:44:21 -0800

    made partial forms work for add/edit

--- TaskList/config/routes.rb
+++ TaskList/config/routes.rb
@@ -3,7 +3,7 @@ root 'tasks#index'
 
 get '/tasks/' => 'tasks#index'
 get	'tasks/new' => 'tasks#new'
-get '/tasks/:id' => 'tasks#show'
+get '/tasks/:id' => 'tasks#show', :as => :task
 post '/tasks/' => 'tasks#create'
 delete '/tasks/:id' => 'tasks#destroy'
 get 'tasks/:id/edit' => 'tasks#edit'
