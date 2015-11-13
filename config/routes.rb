9fa4ca2524e04f769524df32148daf9e148d080b
Sarah <trowbrsa@gmail.com>
2015-11-12 17:01:32 -0800

    Additional css styling for 'create new task page'

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
 
 root 'tasks#index'
 
-#tasks points to the
-  get '/tasks'=> 'tasks#index'
-  get '/tasks/new' => 'tasks#new'
-  get '/tasks/:id' => 'tasks#show'
-  delete '/tasks' => 'tasks#destroy'
-  post '/tasks' => 'tasks#create'
+get '/tasks'=> 'tasks#index'
+get '/tasks/new' => 'tasks#new'
+delete '/tasks' => 'tasks#destroy'
+get '/tasks/:id' => 'tasks#show'
+post '/tasks' => 'tasks#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
