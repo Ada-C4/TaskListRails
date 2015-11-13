caf28940c9b9840510b0d5c91c6d5eaaa81a5ddf
Desiree <desireepoland@gmail.com>
2015-11-13 09:21:31 -0800

    Add whitespace between methods, update ReadMe for Wave 3

--- task-list/app/controllers/tasks_controller.rb
+++ task-list/app/controllers/tasks_controller.rb
@@ -1,28 +1,35 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
     @title= "Task List"
   end
+
   def show
     id = params[:id]
     @task = Task.find(id)
     @title = @task.name
   end
+
   def new
     @title = "Create A New Task"
     @task = Task.new
   end
+
   def create
     @task = Task.create(task_params)
     redirect_to "/"
   end
+
   def destroy
     id = params[:id]
     @task = Task.find(id)
     @task.delete
     redirect_to "/"
   end
+
   def task_params
     params.require(:task).permit(:name, :description, :completed_at)
   end
+  
 end
