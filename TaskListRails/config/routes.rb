0a82f3dae9e18f55985bff01ba47d2fd6e3e24d5
Amy Hunter <amyrhunter@gmail.com>
2015-11-13 14:30:32 -0800

    Added functionality to mark a task complete

--- TaskListRails/config/routes.rb
+++ TaskListRails/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   get 'tasks/' => 'tasks#index'
-  get 'tasks/new' => 'tasks#new'
   get 'tasks/:id' => 'tasks#show'
+  get 'tasks/new' => 'tasks#new'
   post 'tasks/' => 'tasks#create'
+  get 'tasks/:id/edit' => 'tasks#edit'
+  patch 'tasks/1' => 'tasks#update'
   delete 'tasks/:id' => 'tasks#destroy'
 
 
