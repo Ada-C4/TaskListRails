6cf066a0bb895de3114462a2a5c5aeabc7530045
Lauren Granger <lr.granger21@gmail.com>
2015-11-12 16:10:09 -0800

    wave 2 done

--- rails-tasks/config/routes.rb
+++ rails-tasks/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'tasks/:name/edit' => 'tasks#edit'
   post 'tasks/' => 'tasks#create'
   delete 'tasks/:name' => 'tasks#destroy'
+  put 'tasks/:name' => 'tasks#update'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
