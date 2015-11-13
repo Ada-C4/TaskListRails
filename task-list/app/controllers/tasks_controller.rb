928056a974536a11a9f10e71969ef901e36a0302
Tammy <misstamar@gmail.com>
2015-11-13 09:50:09 -0800

    made a working comleted button

--- task-list/app/controllers/tasks_controller.rb
+++ task-list/app/controllers/tasks_controller.rb
@@ -25,11 +25,26 @@ class TasksController < ApplicationController
     Task.destroy(id)
     redirect_to "/"
   end
-  
+
+  def update
+    id = params[:id]
+    @task = Task.find(id)
+    if !params[:completed_at].nil?
+      @task.completed_at = params[:completed_at]
+      redirect_to "/"
+      #update the task comleted date
+      #make instance varibale boolean so the view will know
+    else
+      #it is probably an update of the task ->
+    end
+  end
+
+
+
   private
 
   def task_params
-    params.permit(task:[:name, :description])
+    params.permit(task:[:name, :description, :completed_at])
   end
 
 
