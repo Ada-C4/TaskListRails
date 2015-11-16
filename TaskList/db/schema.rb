cd91f520ee615c746d8471999c9e02ddf4dacfee
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-16 11:37:54 -0800

    Added Person table

--- TaskList/db/schema.rb
+++ TaskList/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111002028) do
+ActiveRecord::Schema.define(version: 20151116193703) do
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.string   "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
