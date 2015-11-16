a70cd266b2351db537a9a1bbf22a363495eeed43
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-11-16 11:56:26 -0800

    Associated tasks with people

--- tasks/db/seeds.rb
+++ tasks/db/seeds.rb
@@ -23,16 +23,16 @@ end
 
 
 tasks = [
-  { name: "The First Task", description: "", completed: random_time },
-  { name: "Go to Brunch", description: "" },
-  { name: "Go to Lunch", description: "", completed: random_time },
-  { name: "Go to Second Lunch", description: "" },
-  { name: "Play Video Games", description: "", completed: random_time },
-  { name: "High Five Somebody You Don't Know", description: "", completed: random_time },
-  { name: "Plant Flowers", description: "", completed: random_time },
-  { name: "Call Mom", description: "" },
-  { name: "She worries, you know.", description: "" },
-  { name: "Nap.", description: "", completed: random_time }
+  { name: "The First Task", description: "", completed: random_time, person_id: 1 },
+  { name: "Go to Brunch", description: "", person_id: 1 },
+  { name: "Go to Lunch", description: "", completed: random_time, person_id: 1 },
+  { name: "Go to Second Lunch", description: "", person_id: 2 },
+  { name: "Play Video Games", description: "", completed: random_time, person_id: 3 },
+  { name: "High Five Somebody You Don't Know", description: "", completed: random_time, person_id: 1 },
+  { name: "Plant Flowers", description: "", completed: random_time, person_id: 4 },
+  { name: "Call Mom", description: "", person_id: 3 },
+  { name: "She worries, you know.", description: "", person_id: 2 },
+  { name: "Nap.", description: "", completed: random_time, person_id: 4 }
 ]
 
 tasks.each do |task|
