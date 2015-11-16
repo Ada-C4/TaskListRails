6044ed66489119a7143a9ac5216e6e52129410bf
Claire <ClaireS29@gmail.com>
2015-11-16 15:58:46 -0800

    Added styling to show people page and added :photo column to people schema with links to pictures

--- rails-tasklist/db/seeds.rb
+++ rails-tasklist/db/seeds.rb
@@ -11,22 +11,23 @@ end
 
 tasks = [
   { name: "The First Task", description: "ta-da", completed_at: random_time, person_id: 2 },
-  { name: "Go to Brunch", description: "hungryy", person_id: 1 },
-  { name: "Go to Lunch", description: "still hungry", completed_at: random_time, person_id: 1 },
-  { name: "Go to Second Lunch", description: "Hobbits!", person_id: 2 },
-  { name: "Play Video Games", description: "funtime", completed_at: random_time, person_id: 3 },
-  { name: "High Five Somebody You Don't Know", description: "duh", completed_at: random_time, person_id: 3 },
-  { name: "Plant Flowers", description: "go light!", completed_at: random_time, person_id: 1 },
-  { name: "Call Mom", description: "just cause", person_id: 4 },
-  { name: "She worries, you know.", description: "momma!", person_id: 4 },
+  { name: "Save England", description: "with yo powers", person_id: 2 },
+  { name: "Go to Lunch", description: "still hungry", completed_at: random_time, person_id: 5 },
+  { name: "Go to Second Lunch", description: "Hobbits!", person_id: 3 },
+  { name: "Solve Crimes", description: "find them clues", completed_at: random_time, person_id: 4 },
+  { name: "Rob the crown jewels", description: "duh", completed_at: random_time, person_id: 5 },
+  { name: "Riddle me", description: "this", completed_at: random_time, person_id: 1 },
+  { name: "Protect the Queen", description: "God save!", person_id: 2 },
+  { name: "Compose some waltzes", description: "da-da-da-", person_id: 4 },
   { name: "Nap.", description: "zzzzzz", completed_at: random_time, person_id: 3 }
 ]
 
 people = [
-  {name: "Irene Adler"},
-  {name: "Mycroft Holmes"},
-  {name: "John Watson"},
-  {name: "Sherlock Holmes"}
+  {name: "Irene Adler", photo: "http://images5.fanpop.com/image/photos/30800000/Sherlock-sherlock-on-bbc-one-30801762-500-667.jpg" },
+  {name: "Mycroft Holmes", photo: "http://img3.wikia.nocookie.net/__cb20130218233027/bakerstreet/images/thumb/0/07/Mark-gatiss.jpg/500px-Mark-gatiss.jpg" },
+  {name: "John Watson", photo: "http://images6.fanpop.com/image/photos/36500000/Dr-John-Watson-Sherlock-BBC-One-image-dr-john-watson-sherlock-bbc-one-36513680-500-300.png"},
+  {name: "Sherlock Holmes", photo: "https://lifebeinggirly.files.wordpress.com/2014/01/sherlock-holmes-bbc.jpg"},
+  {name: "Moriarty", photo: "http://33.media.tumblr.com/tumblr_lxk4fbQ6eb1qeyqjwo1_500.gif"}
 ]
 
 people.each do |person|
