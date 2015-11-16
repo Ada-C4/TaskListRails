ba51e20426e940d7b353fd41bf9f637304fb2f30
Kelly <kelly_devlin@alumni.brown.edu>
2015-11-16 14:26:52 -0800

    add person task display and link from show view

--- task-list/app/controllers/people_controller.rb
+++ task-list/app/controllers/people_controller.rb
@@ -6,4 +6,8 @@ class PeopleController < ApplicationController
   def show
     @person = Person.find(params[:id])
   end
+
+  def tasks
+    @person = Person.find(params[:id])
+  end
 end
--- task-list/config/routes.rb
+++ task-list/config/routes.rb
@@ -29,6 +29,9 @@ Rails.application.routes.draw do
     # show person
     get 'people/:id' => 'people#show', as: :person
 
+    # display person's tasks
+    get 'people/:id/tasks' => 'people#tasks'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
