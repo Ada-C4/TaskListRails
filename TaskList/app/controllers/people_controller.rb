159affde074cec2aa45ed1e338b5a12a1eb6fc97
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-16 15:45:20 -0800

    rendering partials like a fool

--- TaskList/app/controllers/people_controller.rb
+++ TaskList/app/controllers/people_controller.rb
@@ -4,8 +4,11 @@ class PeopleController < ApplicationController
 	end
 	def show
 		@person = Person.find(params[:id])
+		@completed_tasks = @person.completed_tasks
 	end
 	def tasks
 		@person = Person.find(params[:id])
+		@working_tasks = @person.working_tasks
+		@completed_tasks = @person.completed_tasks
 	end
 end
