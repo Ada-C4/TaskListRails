c423069f83392ae8022bf4d191d9edcdeaee6b53
emgord <emgord@gmail.com>
2015-11-16 14:44:42 -0800

    added a task list for each individual linked from their show page

--- task-list/app/controllers/people_controller.rb
+++ task-list/app/controllers/people_controller.rb
@@ -3,4 +3,16 @@ class PeopleController < ApplicationController
   def index
     @people = Person.all
   end
+
+  def show
+      id = params[:id]
+    @person = Person.find(id)
+  end
+
+  def task_show
+    id = params[:id]
+    person = Person.find(id)
+    tasks = person.tasks
+    @tasks = tasks.order(:completed_date)
+  end
 end
--- task-list/config/routes.rb
+++ task-list/config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   patch 'tasks/:id/complete' => 'tasks#complete'
   patch 'tasks/:id' => 'tasks#update'
   get 'people' => 'people#index'
-
+  get 'people/:id' => 'people#show'
+  get 'people/:id/tasks' => 'people#task_show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
