32810cead9b86271375df7895fd467e2c42e7fd5
Amy Hunter <amyrhunter@gmail.com>
2015-11-16 11:50:14 -0800

    Add seeds file of Simpsons family members and create Person model

--- TaskListRails/db/seeds.rb
+++ TaskListRails/db/seeds.rb
@@ -9,6 +9,14 @@ def random_time
   Time.at(rand * Time.now.to_i)
 end
 
+people = [
+  {name: "Homer Simpson", age: 39, occupation: "Nuclear Safety Technician"},
+  {name: "Marge Simpson", age: 34, occupation: "Homemaker"},
+  {name: "Bart Simpson"}, age: 10, occupation: "Student",
+  {name: "Lisa Simpson"}, age: 8, occupation: "Student",
+  {name: "Maggie Simpson", age: 1, occupation: "Baby"}
+]
+
 tasks = [
   { name: "The First Task", description: "", completed_at: random_time },
   { name: "Go to Brunch", description: "" },
