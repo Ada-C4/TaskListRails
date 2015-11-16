ef579d96f68e82406cd4421cd9599ead467acfa8
emgord <emgord@gmail.com>
2015-11-16 11:56:03 -0800

    added model and migration for person object with seed data and connected it to tasks

--- task-list/db/seeds.rb
+++ task-list/db/seeds.rb
@@ -12,18 +12,28 @@ def random_time
 end
 
 tasks = [
-  { name: "The First Task", description: "", completed_date: random_time },
-  { name: "Go to Brunch", description: "" },
-  { name: "Go to Lunch", description: "", completed_date: random_time },
-  { name: "Go to Second Lunch", description: "" },
-  { name: "Play Video Games", description: "", completed_date: random_time },
-  { name: "High Five Somebody You Don't Know", description: "", completed_date: random_time },
-  { name: "Plant Flowers", description: "", completed_date: random_time },
-  { name: "Call Mom", description: "" },
-  { name: "She worries, you know.", description: "" },
-  { name: "Nap.", description: "", completed_date: random_time }
+  { name: "The First Task", description: "", completed_date: random_time, person_id: 1 },
+  { name: "Go to Brunch", description: "", person_id: 1 },
+  { name: "Go to Lunch", description: "", completed_date: random_time, person_id: 1 },
+  { name: "Go to Second Lunch", description: "", person_id: 1 },
+  { name: "Play Video Games", description: "", completed_date: random_time, person_id: 2},
+  { name: "High Five Somebody You Don't Know", description: "", completed_date: random_time, person_id: 2 },
+  { name: "Plant Flowers", description: "", completed_date: random_time, person_id: 2 },
+  { name: "Call Mom", description: "", person_id: 3 },
+  { name: "She worries, you know.", description: "", person_id: 3 },
+  { name: "Nap.", description: "", completed_date: random_time, person_id: 3 }
 ]
 
 tasks.each do |task|
   Task.create task
+end
+
+people = [
+  {name:"Emily"},
+  {name:"Ely"},
+  {name:"Robin"},
+]
+
+people.each do |person|
+  Person.create(person)
 end
