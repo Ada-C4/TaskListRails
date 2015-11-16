e3f3164dda5469841c18e854448345a725b0ab04
noglows <jessica.noglows@outlook.com>
2015-11-16 12:16:45 -0800

    Created Person model. Index page displaying task owner name

--- rails-task-list/db/seeds.rb
+++ rails-task-list/db/seeds.rb
@@ -6,21 +6,31 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+seed_people = [
+  { name: "Jessica", description: "Always hungry", tasks_completed: 0},
+  { name: "Lolita", description: "Music Listener", tasks_completed: 0},
+  { name: "Kepler", description: "Freeloader", tasks_completed: 0}
+]
+
+seed_people.each do |seed|
+  Person.create seed
+end
+
 def random_time
   Time.at(rand * Time.now.to_i)
 end
 
 tasks = [
-  { name: "The First Task", description: "", completed_date: random_time },
-  { name: "Go to Brunch", description: "" },
-  { name: "Go to Lunch", description: "", completed_date: random_time },
-  { name: "Go to Second Lunch", description: "" },
-  { name: "Play Video Games", description: "", completed_date: random_time },
-  { name: "High Five Somebody You Don't Know", description: "", completed_date: random_time },
-  { name: "Plant Flowers", description: "", completed_date: random_time },
-  { name: "Call Mom", description: "" },
-  { name: "She worries, you know.", description: "" },
-  { name: "Nap.", description: "", completed_date: random_time }
+  { name: "The First Task", description: "", completed_date: random_time, person_id: 1 },
+  { name: "Go to Brunch", description: "", person_id: 2 },
+  { name: "Go to Lunch", description: "", completed_date: random_time, person_id: 3 },
+  { name: "Go to Second Lunch", description: "" , person_id: 3},
+  { name: "Play Video Games", description: "", completed_date: random_time, person_id: 1 },
+  { name: "High Five Somebody You Don't Know", description: "", completed_date: random_time, person_id: 3 },
+  { name: "Plant Flowers", description: "", completed_date: random_time, person_id: 2 },
+  { name: "Call Mom", description: "", person_id: 2 },
+  { name: "She worries, you know.", description: "" , person_id: 1},
+  { name: "Nap.", description: "", completed_date: random_time, person_id: 1 }
 ]
 
 tasks.each do |task|
