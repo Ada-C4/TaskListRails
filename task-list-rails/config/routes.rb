ab1fbeec6ea8911880d8a2c183f235f956b7bf5a
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-16 13:32:59 -0800

    people index view

--- task-list-rails/config/routes.rb
+++ task-list-rails/config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   patch 'tasks/:id/complete' => 'tasks#toggle_complete'
   delete 'tasks/:id' => 'tasks#destroy'
 
+  get 'people/' => 'people#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
