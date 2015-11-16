52296cfa340f4747f24c2594b00b2598404525f5
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-11-16 11:51:44 -0800

    Added person_id column to tasks table

--- tasks/db/schema.rb
+++ tasks/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116193337) do
+ActiveRecord::Schema.define(version: 20151116194355) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20151116193337) do
     t.datetime "completed"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "person_id"
   end
+
+  add_index "tasks", ["person_id"], name: "index_tasks_on_person_id"
 
 end
