3f8281ce17216ab6fc82b71def9d15b36ba98479
Wesley Willis <haileyw@gmail.com>
2015-11-16 12:22:24 -0800

    add Person model and relate to Tasks

--- task-list/db/seeds.rb
+++ task-list/db/seeds.rb
@@ -9,17 +9,31 @@ def random_time
   Time.at(rand * Time.now.to_i)
 end
 
+seed_people = [
+  {name: "Shia"},
+  {name: "Hailey"},
+  {name: "David"},
+  {name: "Alex"},
+  {name: "Graham"},
+  {name: "Daniel"},
+
+]
+
+seed_people.each do |seed|
+  Person.create(seed)
+end
+
 tasks = [
-  { name: "The First Task", description: "", completed: random_time },
-  { name: "Go to Brunch", description: "" },
-  { name: "Go to Lunch", description: "", completed: random_time },
-  { name: "Go to Second Lunch", description: "" },
-  { name: "Play Video Games", description: "", completed: random_time },
-  { name: "High Five Somebody You Don't Know", description: "", completed: random_time },
-  { name: "Plant Flowers", description: "", completed: random_time },
-  { name: "Call Mom", description: "" },
-  { name: "She worries, you know.", description: "" },
-  { name: "Nap.", description: "", completed: random_time }
+  { name: "The First Task", description: "", person_id: 2, completed: random_time },
+  { name: "Go to Brunch", description: "", person_id: 3 },
+  { name: "Go to Lunch", description: "", person_id: 4, completed: random_time },
+  { name: "Go to Second Lunch", description: "", person_id: 5 },
+  { name: "Play Video Games", description: "", person_id: 6, completed: random_time },
+  { name: "High Five Somebody You Don't Know", description: "", person_id: 3, completed: random_time },
+  { name: "Plant Flowers", description: "", person_id: 3, completed: random_time },
+  { name: "Call Mom", description: "", person_id: 3, },
+  { name: "She worries, you know.", description: "", person_id: 3, },
+  { name: "Nap.", description: "", person_id: 3, completed: random_time }
 ]
 
 tasks.each do |task|
